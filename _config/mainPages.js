d15eef9bbed3428c69968aa2a817d3980d88d673
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2023-10-20 18:26:21 -0500

    - changed route name

--- _config/mainPages.js
+++ _config/mainPages.js
@@ -4,8 +4,8 @@ export default {
   fillForm: {
     permission: 'iforms.leads.create',
     activated: true,
-    path: '/form/fill-form/:id',
-    name: 'qform.main.fillForm',
+    path: '/form/lead/create/:id',
+    name: 'qform.main.leads.create',
     page: () => import('@imagina/qform/_pages/main/fillForm'),
     layout: () => import('@imagina/qsite/_layouts/master.vue'),
     title: 'iforms.cms.sidebar.adminField',
